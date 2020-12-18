943769f67923c25569c7d6a5afd2eb4179ec87a3
Rémi Castien <remicastien@gmail.com>
2020-12-18 01:02:06 +0100

    css import image.

--- src/Form/AnnoncesType.php
+++ src/Form/AnnoncesType.php
@@ -5,6 +5,8 @@ namespace App\Form;
 use App\Entity\Annonces;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\FileType;
+use Symfony\Component\Form\Extension\Core\Type\MoneyType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
@@ -13,15 +15,29 @@ class AnnoncesType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder
-            ->add('title')
-            ->add('content')
+            ->add('title',TextType::class,[
+                'attr'=>[
+                    'class'=>'form-control form-group',
+                    'placeholder'=>'name'
+                ]])
+            ->add('content',TextType::class,[
+                'attr'=>[
+                    'class'=>'form-control',
+                    'placeholder'=>'La taille,la déscription...',
+                ]
+            ])
             //on ajoute le champ image dans le formulaire
             // Il n'est pas lié a la base de doonés (mapped  false
-            ->add('images', FileType::class,[
+            ->add('images', FileType::class, [
                 'label' => false,
-                'multiple'=> true,
+                'multiple' => true,
                 'mapped' => false,
                 'required' => false,
+            ])
+            ->add('prices', MoneyType::class, [
+                'attr'=>[
+                    'class'=>'form-control'
+                ]
             ])
 
         ;
